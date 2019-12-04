623916e760cadb74af299f6dadd489b4d158ad85
Gerald Versluis <gerald@verslu.is>
2019-12-04 14:48:00 +0100

    Initial commit

---
+++ AnimatedGifForms/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace AnimatedGifForms
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public bool IsPlaying { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindingContext = this;
+        }
+
+        void Button_Clicked(object sender, EventArgs e)
+        {
+            IsPlaying = !IsPlaying;
+            OnPropertyChanged(nameof(IsPlaying));
+        }
+    }
+}
